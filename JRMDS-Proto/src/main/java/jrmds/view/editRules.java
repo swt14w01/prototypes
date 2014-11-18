1778b544d50737868b628436c7856b62cd181e00
poisonnuke <mail@poisonnuke.de>
2014-11-18 16:17:23 +0100

    ä

--- JRMDS-Proto/src/main/java/jrmds/view/editRules.java
+++ JRMDS-Proto/src/main/java/jrmds/view/editRules.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class editRules {
 
 	@RequestMapping(value="/editRules")
-	public String editRules() {
+	public String htmlOutput() {
 		String output="";
 		return output;
 	}
