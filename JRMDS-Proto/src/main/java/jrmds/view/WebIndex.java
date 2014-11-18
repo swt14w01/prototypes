701127f8a02fe094c536d8facc3b1b7142746bd2
poisonnuke <mail@poisonnuke.de>
2014-11-18 16:12:49 +0100

    ä

--- JRMDS-Proto/src/main/java/jrmds/view/WebIndex.java
+++ JRMDS-Proto/src/main/java/jrmds/view/WebIndex.java
@@ -1,17 +1,15 @@
 package jrmds.view;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebIndex {
 
-	@RequestMapping(value="/",method={RequestMethod.POST, RequestMethod.GET})
-	public String index(
-			@RequestParam(required=false, defaultValue="test") String name,
-			@RequestParam(required=true) String action) {
+	@RequestMapping(value="/")
+	public String index() {
 		return "index";
 	}
 }
