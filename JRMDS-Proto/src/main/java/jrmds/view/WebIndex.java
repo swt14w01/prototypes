1618010813601f8eb174d9b2df1ea6fef3591878
Sinthu <s9556748@mail.zih.tu-dresden.de>
2014-11-19 19:55:47 +0100

    BAM. Stephanie OP xD

--- JRMDS-Proto/src/main/java/jrmds/view/WebIndex.java
+++ JRMDS-Proto/src/main/java/jrmds/view/WebIndex.java
@@ -1,15 +1,15 @@
 package jrmds.view;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebIndex {
 
 	@RequestMapping(value="/")
 	public String index() {
-		return "index";
+		return "redirect:/index";
 	}
 }
--- rest-neo4j-prototype/src/main/java/hello/Application.java
+++ rest-neo4j-prototype/src/main/java/hello/Application.java
@@ -1,22 +1,16 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.data.neo4j.core.GraphDatabase;
 
 @Configuration
 @ComponentScan
@@ -31,7 +25,7 @@ public class Application extends Neo4jConfiguration{
 
 	@Bean(destroyMethod = "shutdown")
 	public GraphDatabaseService graphDatabaseService() {
-		return new GraphDatabaseFactory().newEmbeddedDatabase("target/hello.db");
+		return new SpringRestGraphDatabase("http://[::1]:7474/db/data");
 	}
 
     public static void main(String[] args) throws Exception {
